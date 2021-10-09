988e5cf5a44afd792dbe84411c6da5a358216f3d
korchasa <korchasa@icloud.com>
2021-10-09 15:50:05 +0300

    feat: enable centos7 in ./test.sh

--- test.sh
+++ test.sh
@@ -4,5 +4,5 @@ set -ex
 docker build --tag ruchki-lint -f ./lint.Dockerfile .
 docker run ruchki-lint
 
-#docker build --tag ruchki-centos7 -f ./centos7.Dockerfile .
-#docker run ruchki-centos7
+docker build --tag ruchki-centos7 -f ./centos7.Dockerfile .
+docker run ruchki-centos7
