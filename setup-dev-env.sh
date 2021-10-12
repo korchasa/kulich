3c9cbe4f96b23ba330c9d201297422f69e187ce7
korchasa <korchasa@icloud.com>
2021-10-13 02:52:29 +0300

    fix: ignore local dev files

--- setup-dev-env.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-docker build --progress plain --no-cache --tag ruchki-centos7-dev . -f ./centos7.Dockerfile
-docker run --detach --rm -v "$(pwd)":/work --name ruchki-centos7-dev ruchki-centos7-dev
-docker exec -it ruchki-centos7-dev bash
-
--- sync.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-while :
-do
-	rsync -avz ./ hetzner-centos7:/root/ruchki/
-	sleep 3
-done
